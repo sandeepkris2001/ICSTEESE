b330c753bd18d91bce5b506634ae8304dbdae3cd
SANDEEP K <87005602+sandeepkris2001@users.noreply.github.com>
2022-12-23 20:57:46 +0530

    Add files via upload

---
+++ ICSTEESE/app.js
@@ -0,0 +1,16 @@
+const adc = document.getElementById("article1");
+const tc = document.getElementById("article2");
+const oc = document.getElementById("article3");
+
+adc.style.display = "none";
+function adcDisplay(){
+    adc.style.display= "inherit";
+}
+tc.style.display = "none";
+function tcDisplay(){
+    tc.style.display= "inherit";
+}
+oc.style.display = "none";
+function ocDisplay(){
+    oc.style.display= "inherit";
+}
